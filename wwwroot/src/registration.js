e2643d8bc73f326122cd8304e3cf1d8d750d53e5
d <d>
2023-04-23 11:50:24 +0300

    Test

--- wwwroot/src/registration.js
+++ wwwroot/src/registration.js
@@ -11,60 +11,82 @@ closePopUp.addEventListener("click", () => {
   popUp.classList.remove("active");
 });
 
-const form = document.querySelector(".js-form");
-const submitBtn = form.querySelector("button");
+// const form = document.querySelector(".js-form");
+// const submitBtn = form.querySelector("button");
 
-const nameInput = form.querySelector('input[type="text"][placeholder="Ім\'я"]');
-const surnameInput = form.querySelector(
-  'input[type="text"][placeholder="Прізвище"]'
-);
-const phoneInput = form.querySelector(
-  'input[type="tel"][placeholder="Телефон"]'
-);
-const emailInput = form.querySelector(
-  'input[type="email"][placeholder="Емейл"]'
-);
-const passwordInput = form.querySelector(
-  'input[type="password"][placeholder="Пароль"]'
-);
+// const nameInput = form.querySelector('input[type="text"][placeholder="Ім\'я"]');
+// const surnameInput = form.querySelector(
+//   'input[type="text"][placeholder="Прізвище"]'
+// );
+// const phoneInput = form.querySelector(
+//   'input[type="tel"][placeholder="Телефон"]'
+// );
+// const emailInput = form.querySelector(
+//   'input[type="email"][placeholder="Емейл"]'
+// );
+// const passwordInput = form.querySelector(
+//   'input[type="password"][placeholder="Пароль"]'
+// );
 
-form.addEventListener("submit", function (e) {
+// form.addEventListener("submit", function (e) {
+//   e.preventDefault();
+
+//   if (
+//     nameInput.value === "" ||
+//     surnameInput.value === "" ||
+//     phoneInput.value === "" ||
+//     emailInput.value === "" ||
+//     passwordInput.value === ""
+//   ) {
+//     alert("Будь ласка, заповніть усі поля форми!");
+//     return;
+//   }
+
+//   if (!/^\d+$/.test(phoneInput.value)) {
+//     alert("Будь ласка, введіть лише цифри у полі телефон!");
+//     return;
+//   }
+
+//   if (
+//     !/^(?=.*[A-Z])[a-zA-Z0-9~`!@#$%^&*()_+={[}\]|:;"'<,>.?/]+$/.test(
+//       passwordInput.value
+//     )
+//   ) {
+//     alert(
+//       "Пароль повинен містити хоча б одну велику латинську літеру і складатися лише з латинських та додаткових символів!"
+//     );
+//     return;
+//   }
+
+//   form.submit();
+// });
+
+// phoneInput.addEventListener("input", function () {
+//   this.value = this.value.replace(/[^0-9]/g, "");
+// });
+
+// passwordInput.addEventListener("input", function () {
+//   this.value = this.value.toUpperCase();
+// });
+
+
+document.getElementById("registerForm").addEventListener("click", async e => {
   e.preventDefault();
-
-  if (
-    nameInput.value === "" ||
-    surnameInput.value === "" ||
-    phoneInput.value === "" ||
-    emailInput.value === "" ||
-    passwordInput.value === ""
-  ) {
-    alert("Будь ласка, заповніть усі поля форми!");
-    return;
-  }
-
-  if (!/^\d+$/.test(phoneInput.value)) {
-    alert("Будь ласка, введіть лише цифри у полі телефон!");
-    return;
-  }
-
-  if (
-    !/^(?=.*[A-Z])[a-zA-Z0-9~`!@#$%^&*()_+={[}\]|:;"'<,>.?/]+$/.test(
-      passwordInput.value
-    )
-  ) {
-    alert(
-      "Пароль повинен містити хоча б одну велику латинську літеру і складатися лише з латинських та додаткових символів!"
-    );
-    return;
-  }
-
-  form.submit();
-});
-
-phoneInput.addEventListener("input", function () {
-  this.value = this.value.replace(/[^0-9]/g, "");
-});
-
-passwordInput.addEventListener("input", function () {
-  this.value = this.value.toUpperCase();
+  // отправляет запрос и получаем ответ
+  const response = await fetch("/api/auth/signup", {
+      method: "POST",
+      headers: { "Accept": "application/json", "Content-Type": "application/json" },
+      body: JSON.stringify({
+          name: document.getElementById("name").value,
+          password: document.getElementById("password").value
+      })
+  });
+  // если запрос прошел нормально
+  if (response.ok === true) {
+    alert("Registration successful");
+    // const data = await response.json();
+    // sessionStorage.setItem(tokenKey, data.access_token);
+}
+else  // если произошла ошибка, получаем код статуса
+    console.log("Status: ", response.status);
 });
